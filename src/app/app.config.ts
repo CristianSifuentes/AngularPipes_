1d326e1b87e532468350f0cc08ae65b1d9e30cd0
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2025-10-25 17:36:52 -0600

    Working with pipes, Tailwind v4 and daisyUI v5

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,20 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+ApplicationConfig,
+provideBrowserGlobalErrorListeners,
+provideZonelessChangeDetection
+ } from '@angular/core';
+
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { registerLocaleData } from '@angular/common';
+
+import localeEs from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
+import { LocaleService } from './services/locale.service';
+
+registerLocaleData(localeEs, 'es');
+registerLocaleData(localeFr, 'fr');
 
 export const appConfig: ApplicationConfig = {
   providers: [
